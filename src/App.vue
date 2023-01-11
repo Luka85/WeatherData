48d307c5a394f75b5d36b062c43fdb38eaebd6fa
Luka Kikelj <kikelj.luka@gmail.com>
2023-01-11 08:04:40 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <the-header title="Weather Data"></the-header>
+</template>
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #2e635e;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  color: #fff;
+  font-weight: 200;
+}
+</style>
